42c2a1644a5a4bdaf63f20eafec5b4f2fafe124d
RedEyeDragoon <ximaginebreaksx@gmail.com>
2021-11-21 13:51:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qxsupl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qxsupl)
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CharacterItem from './CharacterItem';
+
+const CharacterGrid = ({ items, isLoading }) => {
+  return(isLoading ? (<h1>Loading...</h1>) : (<section className='cards'>
+    {items.map(character => (
+      <CharacterItem
+        key={character.char_id}
+        character={character}
+      />
+  ))}
+  </section>))  
+}
+
+export default CharacterGrid;
